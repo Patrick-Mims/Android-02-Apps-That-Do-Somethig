b247ceca00de3c99e69e18adc8be51d1a3d6dc05
weatherpub <patrick@bayareamedia.net>
2023-10-02 22:31:57 -0700

    Implemented DataThread

--- app/src/main/java/edu/sfsu/app/Task/DataTask.java
+++ app/src/main/java/edu/sfsu/app/Task/DataTask.java
@@ -1,4 +1,66 @@
 package edu.sfsu.app.Task;
 
-public class DataTask {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ProgressBar;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import edu.sfsu.app.model.DrinkModel;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DataTask extends AsyncTask<String, Integer, String> {
+    @SuppressLint("StaticFieldLeak")
+    Context context;
+    @SuppressLint("StaticFieldLeak")
+    RecyclerView recyclerView;
+    @SuppressLint("StaticFieldLeak")
+    ProgressBar progressBar;
+    ArrayList<DrinkModel> model;
+    private final OkHttpClient client = new OkHttpClient();
+    public DataTask(Context context, RecyclerView recyclerView, ProgressBar progressBar, ArrayList<DrinkModel> model) {
+        Log.v("LOG", "DataTask");
+        this.context = context;
+        this.recyclerView = recyclerView;
+        this.progressBar = progressBar;
+        this.model = model;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Log.v("LOG", "onPreExecute");
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.v("LOG", "RUN()");
+                Response body = null;
+                Request request = new Request.Builder().url(params[0]).build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    if(response.isSuccessful()) {
+                        Log.v("LOG", "Response => " + response.body().string());
+                        response.body().close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+
+        return null;
+    }
 }
---
+++ app/src/main/java/edu/sfsu/app/Task/DataThread.java
@@ -0,0 +1,46 @@
+package edu.sfsu.app.Task;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class DataThread extends Thread {
+    String api;
+    private static final OkHttpClient client = new OkHttpClient();
+    public DataThread(String api) {
+        this.api = api;
+        Log.v("LOG", "DataThread()");
+        // The call to setDaemon(), with the argument true, makes the thread that is created a daemon thread.
+        // A Daemon thread is simple a background thread that is subordinate to the thread that created it.
+        setDaemon(true);
+    }
+    // Synchronous Request
+    public void getData(String api) throws InterruptedException {
+        final Thread thread = new Thread(new Runnable() {
+            // The run() method contains the code for thread execution
+            @Override
+            public void run() {
+                Log.v("LOG", "run()");
+
+                Request request = new Request.Builder().url(api).build();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    if(response.isSuccessful()) {
+                        assert response.body() != null;
+                        Log.v("LOG", "Response => " + response.body().string());
+                        response.body().close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        thread.join();
+    }
+}
